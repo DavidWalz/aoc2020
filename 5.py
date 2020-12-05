7abcbd11f8a313d06a961b96bb01b16e229e6f92
WalzDS <david-simon.walz@basf.com>
2020-12-05 19:53:13 +0100

    simplify day 5

--- 5.py
+++ 5.py
@@ -1,28 +1,25 @@
 # https://adventofcode.com/2020/day/5
-# Row and column numbers can be recovered from the binary partitioning by replacing
+# The seat ID can be recovered from the binary partitioning by replacing
 # the characters F/R to 0 and B/L to 1 and reading the code in base 2.
 import numpy as np
 
 lines = open("5.txt").read().split("\n")
 
 
-def seat(line):
+def seatID(line):
     for r in (("F", "0"), ("B", "1"), ("R", "1"), ("L", "0")):
         line = line.replace(*r)
-    row = int(line[:7], 2)
-    col = int(line[7:], 2)
-    return [row, col]
+    return int(line, 2)
 
-def seatID(row, col):
-    return row * 8 + col
 
-rows, cols = np.array(list(map(seat, lines))).T
+ids = np.array(list(map(seatID, lines)))
 
 # part 1: find maximum seat ID
-print(max(seatID(rows, cols)))
+print(max(ids))
 
-# part2: find the empty seat with non-empty seats next to it
+# part 2: find the one empty seat with only non-empty seats next to it
 A = np.zeros((128, 8), bool)
-A[rows, cols] = 1
-B = ~A & np.roll(A, 1, axis=0) & np.roll(A, -1, 0) & np.roll(A, 1, 1) & np.roll(A, -1, 1)
-print(seatID(*np.where(B)))
+A[ids // 8, ids % 8] = 1
+B = ~A & np.roll(A, 1, 0) & np.roll(A, -1, 0) & np.roll(A, 1, 1) & np.roll(A, -1, 1)
+row, col = np.where(B)
+print(row * 8 + col)
