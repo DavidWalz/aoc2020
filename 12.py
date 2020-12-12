d53ebe76b69c93820758616472f57f35ada1aeb4
WalzDS <david-simon.walz@basf.com>
2020-12-12 14:26:38 +0100

    day 12: switch to complex numbers

--- 12.py
+++ 12.py
@@ -1,58 +1,36 @@
 # https://adventofcode.com/2020/day/12
 
-lines = open("12.txt").read().split("\n")
+navigation = [(line[0], int(line[1:].strip())) for line in open("12.txt").readlines()]
+
+dirs = {"N": 1j, "S": -1j, "E": 1, "W": -1}
+turn = {"L": 1j, "R": -1j}
 
 
 # part 1
-heading = 90
-compass = {"N": 0, "E": 90, "S": 180, "W": 270}
-travelled = {0: 0, 90: 0, 180: 0, 270: 0}
+pos = 0 + 0j
+dir = 1 + 0j
 
-for line in lines:
-    h = line[0]
-    d = int(line[1:])
-    if h in ("L", "R"):  # turn
-        sign = +1 if h == "R" else -1
-        heading = (heading + sign * d) % 360
-    elif h == "F":  # move along current heading
-        travelled[heading] += d
-    else:  # move in stated direction
-        travelled[compass[h]] += d
+for cmd, val in navigation:
+    if cmd in dirs:
+        pos += val * dirs[cmd]
+    elif cmd in turn:
+        dir *= turn[cmd] ** (val // 90)
+    elif cmd == "F":
+        pos += val * dir
 
-print(abs(travelled[0] - travelled[180]) + abs(travelled[90] - travelled[270]))
+print(abs(pos.real) + abs(pos.imag))
 
 
 # part 2
-pe, pn = 0, 0  # ship position in easting / northing
-we, wn = 10, 1  # waypoint, relative position in easting / northing
+pos = 0 + 0j
+wp = 10 + 1j
 
-for line in lines:
-    h = line[0]
-    d = int(line[1:])
+for cmd, val in navigation:
+    if cmd in dirs:
+        wp += val * dirs[cmd]
+    elif cmd in turn:
+        wp *= turn[cmd] ** (val // 90)
+    elif cmd == "F":
+        pos += val * wp
 
-    # move waypoint
-    if h == "N":
-        wn += d
-    if h == "S":
-        wn -= d
-    if h == "E":
-        we += d
-    if h == "W":
-        we -= d
-
-    # rotate waypoint
-    if h in ("L", "R"):
-        sign = +1 if h == "R" else -1
-        if d == 90:
-            we, wn = wn * sign, -we * sign
-        if d == 180:
-            we, wn = -we, -wn
-        if d == 270:
-            we, wn = -wn * sign, we * sign
-
-    # move towards waypoint
-    if h == "F":
-        pe += d * we
-        pn += d * wn
-
-print(abs(pe) + abs(pn))
+print(abs(pos.real) + abs(pos.imag))
